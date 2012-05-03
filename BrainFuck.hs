478140ad10ab411aeb6ffcfb7a5ceab2c5366b69
Mike Blume <mike@loggly.com>
2012-05-03 10:38:09 -0700

    some shit I did

--- BrainFuck.hs
+++ BrainFuck.hs
@@ -1,15 +1,42 @@
 import Tape
+import Control.Monad.Reader
 
-doCommand :: Char -> Tape -> IO Tape
-doCommand '<' = return . retreat
-doCommand '>' = return . advance
-doCommand '+' = return . increment
-doCommand '-' = return . decrement
-doCommand '.' tape = do
-    c <- getChar
-    return writeTape tape c
-doCommand ',' tape = do
-    putChar $ readTape tape
-    return tape
-doCommand
+doCommand :: Char -> BFMon ()
+doCommand '<' = modTape retreat
+doCommand '>' = modTape advance
+doCommand '+' = modTape increment
+doCommand '-' = modTape decrement
+doCommand '.' = getChar >>= (modTape . writeTape)
+doCommand ',' = readTapeM >>= putChar
+doCommand '[' = do
+  ts <- readTapeM
+  if ts == 0
+    then doJump
+    else return ()
+doCommand ']' = do
+  ts <- readTapeM
+  if ts == 0
+    then return ()
+    else doJump
 
+lookupJump :: JumpTable -> Int -> Int
+lookupJump ((a, b): js) n
+  | a == n = b
+  | b == n = a
+  | otherwise = lookupJump js n
+
+type JumpTable = [(Int, Int)]
+type BFProg = [Char]
+
+type BFRead a = Reader (BFProg, JumpTable) a
+
+lookupJumpM :: Int -> BFRead Int
+lookupJumpM i = do
+  (_prog, jt) <- ask
+  return $ lookupJump jt i
+
+doJump :: BFMon ()
+doJump = do
+  ip <- getIP
+  ip' <- lookupJumpM ip
+  setIP ip'
--- Tape.hs
+++ Tape.hs
@@ -1,5 +1,6 @@
-module Tape (Tape, advance, retreat, increment
-            ,decrement, readTape, writeTape, blankTape) where
+module Tape where
+
+import Control.Monad.State
 
 data Tape = Tape [Int] Int [Int]
 
@@ -13,9 +14,36 @@ increment (Tape l c r) = Tape l (c + 1) r
 
 decrement (Tape l c r) = Tape l (c - 1) r
 
-writeTape (Tape l c r) nc = Tape l nc r
+writeTape nc (Tape l c r) = Tape l nc r
 
 readTape (Tape _ c _) = c
 
 blankTape = Tape [] 0 []
+
+type BFState a = State (Tape, Int) a
+
+modTape :: (Tape -> Tape) -> BFState ()
+modTape tf = do
+  (tape, ip) <- get
+  put (tf tape, ip)
+
+readTapeM :: BFState Int
+readTapeM = do
+  (tape, _ip) <- get
+  return $ readTape tape
+
+getIP :: BFState Int
+getIP = do
+  (_tape, ip) <- get
+  return ip
+
+setIP :: Int -> BFState ()
+setIP n = do
+  (tape, _ip) <- get
+  put (tape, n)
+
+incIP :: BFState ()
+incIP = do
+  (tape, ip) <- get
+  put (tape, ip + 1)
 
